0c23667f57bb2282e6e6b3c14a01103e31ee55ce
T.C. <thomas.n.cool@gmail.com>
2015-06-09 15:44:19 -0700

    Update cut-the-sticks.py

--- cut-the-sticks.py
+++ cut-the-sticks.py
@@ -3,7 +3,7 @@ def cut_sticks(n,s):
         for _ in range(n):
             if len(l) > 0:
                 print len(l)
-                l = [x - min(s) for x in s if x - min(s) > 0]
+                s = [x - min(s) for x in s if x - min(s) > 0]
 
 num = int(raw_input().strip())
 
